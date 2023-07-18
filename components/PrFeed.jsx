bb1de047e18ae48e5fab1f206be4a7116b77f1f3
JadePablo <98762929+JadePablo@users.noreply.github.com>
2023-07-18 01:31:34 -0400

    adds filtering feature on pr page

--- components/PrFeed.jsx
+++ components/PrFeed.jsx
@@ -1,42 +1,97 @@
 "use client";
 
-import React from 'react'
-import { Container ,Grid} from '@mui/material';
-import { useEffect, useState } from 'react';
-import { usePathname,redirect } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, Select, MenuItem, Box } from '@mui/material';
+import { usePathname } from 'next/navigation';
 import PrCard from './PrCard';
 
 const PrFeed = () => {
+  const [prs, setPrs] = useState([]);
+  const [selectedLift, setSelectedLift] = useState('');
+  const [selectedSort, setSelectedSort] = useState('');
+  const pathname = usePathname();
 
-    const [prs,setPrs] = useState([])
-    const pathname = usePathname();
+  useEffect(() => {
+    const fetchPrs = async () => {
+      const response = await fetch(`api/${pathname.slice(1).replace(/%20/g, ' ')}`);
+      const data = await response.json();
+      setPrs(data);
+    };
 
-    useEffect(() => {
-        const fetchPrs = async () => {
-            const response = await fetch(`api/${pathname.slice(1).replace(/%20/g, ' ')}`)
-            const data = await response.json();
+    fetchPrs();
+  }, []);
 
-            setPrs(data);
-            
-        };
+  const handleLiftChange = (event) => {
+    setSelectedLift(event.target.value);
+  };
 
-        fetchPrs();
-    },[])
+  const handleSortChange = (event) => {
+    setSelectedSort(event.target.value);
+  };
+
+  const filteredPrs = prs.filter((pr) => {
+    if (selectedLift && pr.lift !== selectedLift) {
+      return false;
+    }
+    return true;
+  });
+
+  const sortedPrs = filteredPrs.sort((a, b) => {
+    if (selectedSort === 'ascDate') {
+      return new Date(a.date) - new Date(b.date);
+    }
+    if (selectedSort === 'descDate') {
+      return new Date(b.date) - new Date(a.date);
+    }
+    if (selectedSort === 'ascWeight') {
+      return a.weight - b.weight;
+    }
+    if (selectedSort === 'descWeight') {
+      return b.weight - a.weight;
+    }
+    return 0;
+  });
+
   return (
     <Container>
-        <Grid>
-        {
-          prs.map(pr => {
-            return(
-                <Grid item sx={{margin:"1em"}}key={pr._id} xs={12} sm={6} md={6}>
-                    <PrCard lift={pr.lift} lifter={pr.lifter} weight={pr.weight} date={pr.date} source={pr.source}/>
-                </Grid>
-            )
-          })
-        }
-        </Grid>
-    </Container>
-  )
-}
+      <Box sx={{ display: 'flex', justifyContent: 'center', gap: '1em', marginTop: '1em', marginBottom: '1em' }}>
+        <Select
+          value={selectedLift}
+          onChange={handleLiftChange}
+        >
+          <MenuItem value="">All Lifts</MenuItem>
+          <MenuItem value="bench">Bench</MenuItem>
+          <MenuItem value="squat">Squat</MenuItem>
+          <MenuItem value="deadlift">Deadlift</MenuItem>
+        </Select>
 
-export default PrFeed
+        <Select
+          value={selectedSort}
+          onChange={handleSortChange}
+        >
+          <MenuItem value="">Sort By</MenuItem>
+          <MenuItem value="ascDate">Date (Ascending)</MenuItem>
+          <MenuItem value="descDate">Date (Descending)</MenuItem>
+          <MenuItem value="ascWeight">Weight (Ascending)</MenuItem>
+          <MenuItem value="descWeight">Weight (Descending)</MenuItem>
+        </Select>
+      </Box>
+
+      <Grid container spacing={2}>
+        {sortedPrs.map((pr) => (
+          <Grid item xs={12} sm={6} md={6} key={pr._id}>
+            <PrCard
+              lift={pr.lift}
+              lifter={pr.lifter}
+              weight={pr.weight}
+              date={pr.date}
+              source={pr.source}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default PrFeed;
