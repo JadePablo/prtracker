865e5c52abf6494fa15fadda79ea1a3c7b1004fe
JadePablo <98762929+JadePablo@users.noreply.github.com>
2023-07-19 08:48:55 -0400

    adds judge emailing system for newly posted prs

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -92,11 +92,43 @@ function EmailButton() {
     }
   };
 
+  const handleNotifyClick = async () => {
+    const mock_pr = {
+      lift: 'bench',
+      weight: 200,
+      location: 'alphaland',
+      lifter: 'John Doe',
+      verified: true,
+      lifterEmail: 'jadepablo97@gmail.com',
+      beaten: false
+    };
+    try {
+      const response = await fetch('/api/send-email/notify', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({pr: mock_pr})
+      });
+
+      if (response.ok) {
+        console.log('Ban email sent successfully');
+      } else {
+        console.error('Failed to send ban email');
+      }
+    } catch (error) {
+      console.error('Error sending ban email:', error);
+    }
+  };
+
   return (
     <div>
       <button onClick={handleVerifyClick}>Verify</button>
       <button onClick={handleRejectClick}>Reject</button>
       <button onClick={handleBanClick}>Ban</button>
+      <button onClick={handleNotifyClick}>Notify</button>
+
+
     </div>
   );
 }
--- email_helpers/notifyJudge.js
+++ email_helpers/notifyJudge.js
@@ -1,12 +1,13 @@
+import auto_email from "./auto_email";
+
 export default async function notifyJudge(email, pr) {
     const judgeMessage = `
     New Pr Submitted
     -------------------
     Info:
-    ${pr}
+    ${JSON.stringify(pr, null, 2)}
     `;
 
     const email_response = await auto_email(email, judgeMessage);
-
     return email_response;
 }
