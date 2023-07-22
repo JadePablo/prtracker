c0ea147b0f444cdb9aa29bf3196d3642328cac2c
JadePablo <98762929+JadePablo@users.noreply.github.com>
2023-07-22 12:36:45 -0400

    add app icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,22 @@ import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 
 //put metadata here
-
+export const metadata = {
+  title: 'PRTracker',
+  description: 'compare your prs with other people',
+  icons: {
+    icon: [
+      '/favicon.ico?v=4',
+    ],
+    apple: [
+      '/apple-touch-icon.png?v=4'
+    ],
+    shortcut: [
+      '/apple-touch-icon.png'
+    ]
+  },
+  manifest: '/site.webmanifest'
+}
 const RootLayout = ({children}) => {
   return (
     <html lang="en">
